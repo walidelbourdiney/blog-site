db5bc0939e67684a68ea5417e44f40eb11c4ac48
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-10-24 17:39:29 +0300

    init blog-React files

---
+++ src/componenets/Promo.js
@@ -0,0 +1,15 @@
+function Promo() {
+  return (
+    <div className="promo-section">
+      <>
+        <h1>Don't miss this deal!</h1>
+
+        <h2>
+          Subscribe to my newsletter and get all the shop items at 50% off!
+        </h2>
+      </>
+    </div>
+  );
+}
+
+export default Promo;
